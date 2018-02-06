e156d4584745e537a70f7e7780eda0dc494e9690
jbhyun <jbhyun@cern.ch>
2018-02-06 19:32:56 +0900

    Code Suggestion for New Skim

--- LQAnalysis/SKTreeMaker/include/SKTreeMaker_LinkDef.h
+++ LQAnalysis/SKTreeMaker/include/SKTreeMaker_LinkDef.h
@@ -8,6 +8,7 @@
 
 #pragma link C++ class SKTreeMaker+;
 #pragma link C++ class SKTreeMakerDiLep+;
+#pragma link C++ class SKTreeMakerDiLep_Trig+;
 #pragma link C++ class SKTreeMakerHNDiLep+;
 #pragma link C++ class SKTreeMakerHNFatJet+;
 #pragma link C++ class SKTreeMakerFakeHN+;
